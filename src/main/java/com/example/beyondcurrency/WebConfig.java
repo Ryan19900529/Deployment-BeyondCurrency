b3bb4520eac5f45a73dc5731a1e632b681973ee6
Ryan <ab00999819@gmail.com>
2024-07-29 20:34:17 -0700

    rm ssl2

--- src/main/java/com/example/beyondcurrency/WebConfig.java
+++ src/main/java/com/example/beyondcurrency/WebConfig.java
@@ -2,15 +2,17 @@ package com.example.beyondcurrency;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://beyondcurrency-deployment-production.up.railway.app") // Replace with your specific domain
+                .allowedOrigins("https://beyondcurrency-deployment-production.up.railway.app/") // Replace with your specific domain
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
